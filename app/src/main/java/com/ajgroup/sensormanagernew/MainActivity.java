45d24ed9431fc6d271d122c67a5a6730b2e46fd6
qoryalfina7@gmail.com <daffa20dappo@gmail.com>
2022-01-09 21:46:42 +0700

    Sensor-Pressure App

---
+++ app/src/main/java/com/ajgroup/sensormanagernew/MainActivity.java
@@ -0,0 +1,61 @@
+package com.ajgroup.sensormanagernew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    Button Tampil, SensorP;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SensorP = findViewById(R.id.sensor);
+
+        Tampil = findViewById(R.id.tampil);
+        Tampil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        SensorP = findViewById(R.id.sensor);
+        SensorP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PressureSensor.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
